02c4bd286cb653244dafabd2a3de6c9b56eb4f70
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2019-08-20 11:09:39 -0500

    Últimos cambios, ahora sí. Eso espero...

--- ProyectoFinal/ProyectoFinal.c
+++ ProyectoFinal/ProyectoFinal.c
@@ -1,16 +1,8 @@
 /**
- * Programa que carga en un PIC un sistema de autenticación por clave.
+ * Proyecto final de Microprocesadores.
  * 
  * @author Stiven Muñoz Murillo
- * @version 16/07/2019
- * 
- * 
- * @autot: el loko
- * @version 16/07/2019
- * Nota: El loco se propone a crear un metodo que pueda leer y escribir de la 
- * EEPROM
- * 
- * 
+ * @version 19/08/2019
  */
 
 #pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
@@ -30,202 +22,318 @@
 #define _XTAL_FREQ 4000000
 
 int contador = 0;
+int contadorErrores = 0;
+int banderaNueva = 0;
+char teclas[] = {' ', ' ', ' ', ' '};
 
-void pulso() {
-    RA4 = 0;
-    RA4 = 1;
+void transmitirDato(int dato) {
+    TXREG = dato;
+    TXEN = 1;
+    while (TXIF == 0) {
+        ;
+    }
+}
+
+void transmitirMensaje(char mensaje[]) {
+    for (int i = 0; i < strlen(mensaje); i++) {
+        transmitirDato(mensaje[i]);
+    }
+
+    transmitirDato(10);
+    transmitirDato(13);
+}
+
+/**
+ * Envia un Pulso al LCD.
+ */
+void enviarPulso() {
+    RB3 = 0;
+    RB3 = 1;
     __delay_ms(15);
-    RA4 = 0;
+    RB3 = 0;
 }
 
-void comando(int c) {
-    PORTC = c;
-    RA5 = 0;
-    pulso();
+/**
+ * Envia un Comando al LCD.
+ * 
+ * @param comando
+ */
+void enviarComando(int comando) {
+    PORTC = comando;
+    RB2 = 0;
+    enviarPulso();
 }
 
-void dato(int d) {
-    PORTC = d;
-    RA5 = 1;
-    pulso();
+/**
+ * Envia un Dato para escribir en el LCD.
+ * 
+ * @param dato
+ */
+void enviarDato(int dato) {
+    PORTC = dato;
+    RB2 = 1;
+    enviarPulso();
+}
+
+/**
+ * Función que pinta asteriscos.
+ * 
+ * @param cantidad
+ */
+void pintarAsteriscos(int cantidad) {
+    for (int i = 0; i <= cantidad; i++) {
+        enviarComando(128 + i); // Set DD RAM address
+        enviarDato('*');
+    }
 }
 
 /**
  * Muestra un mensaje de menos de 16 caracteres.
+ * 
  * @param mensaje
  */
 void mensajeCorto(char mensaje[]) {
     for (int i = 0; i < strlen(mensaje); i++) {
-        comando(128 + i); // Set DD RAM address
-        dato(mensaje[i]);
+        enviarComando(128 + i); // Set DD RAM address
+        enviarDato(mensaje[i]);
     }
 }
 
+/**
+ * Función que escribe en la EEPROM.
+ * 
+ * @param Address, dirección del bloque de memoria donde se va a escribir.
+ * @param Data, caracter que se va escribir en dicho bloque de memoria.
+ */
+void EEPROM_Write(int Address, char Data) {
+    while (EECON1bits.WR); // Waits Until Last Attempt To Write Is Finished
+    EEADR = Address; // Writes The Addres To Which We'll Wite Our Data
+    EEDATA = Data; // Write The Data To Be Saved
+    EECON1bits.EEPGD = 0; // Cleared To Point To EEPROM Not The Program Memory
+    EECON1bits.WREN = 1; // Enable The Operation !
+    INTCONbits.GIE = 0; // Disable All Interrupts Untill Writting Data Is Done
+    EECON2 = 0x55; // Part Of Writing Mechanism..
+    EECON2 = 0xAA; // Part Of Writing Mechanism..
+    EECON1bits.WR = 1; // Part Of Writing Mechanism..
+    INTCONbits.GIE = 1; // Re-Enable Interrupts
+    EECON1bits.WREN = 0; // Disable The Operation
+    EECON1bits.WR = 0; // Ready For Next Writting Operation
+}
 
-
-//Metodo del loco
-//Donde es la posicion de la EEPROM 
-//dato es el dato que se va a meter
-void meterloEnLaEEPROM(int donde, int dato){
-    mensajeCorto("Metiendo en la EEPROM");
-    
-    //Si esta listo para escribir
-    if(EECON1bits.WR == 0){
-        
-        EEADR = donde; // posicion en la que voy a escribir
-        EEDATA = dato; // que le voy a escribir
-        
-        //Habilito para escritura
-        EECON1bits.EEPGD = 0;
-        EECON1bits.WREN = 1;
-        
-        //Paso obligatorio
-        GIE = 0; //desactivo las interrupciones
-        EECON2 = 85; //alisto motores para escritura
-        EECON2 = 170; 
-        EECON1bits.WR = 1; //Le digo que estoy escribiendo
-        
-        //Habilito interrupciones
-        GIE = 1;
-        //Desabilito la escritura
-        EECON1bits.WREN = 0;
-        
-    }
-    
-            
+/**
+ * Función que lee de la EEPROM.
+ * 
+ * @param Address, dirección del bloque de memoria de donde voy a leer.
+ * @return Contenido de dicho bloque de memoria.
+ */
+int EEPROM_Read(int Address) {
+    int Data;
+    EEADR = Address; // Write The Address From Which We Wonna Get Data
+    EECON1bits.EEPGD = 0; // Cleared To Point To EEPROM Not The Program Memory
+    EECON1bits.RD = 1; // Start The Read Operation
+    Data = EEDATA; // Read The Data
+    return Data;
 }
 
 void __interrupt() listener() {
     int leer = PORTA;
     leer = PORTB;
 
-    INTF = 0;
-    RBIF = 0;
+    INTF = 0; // Bandera de RB0
+    RBIF = 0; // Bandera del Puerto B
 
     if (RB0 == 1) {
+        enviarComando(1);
         contador = 0;
-        mensajeCorto("Borrando!!!");
+        contadorErrores = 0;
+        mensajeCorto("Unlock");
+        SPEN = 1; // Habilitador del puerto serial
+        transmitirMensaje(" SecurityBox Unlock ");
+        SPEN = 0; // Habilitador del puerto serial
         __delay_ms(1000);
-        comando(1);
+        enviarComando(1);
     }
-    
-    if (RB1 == 1) {
-        contador = 0;
-        mensajeCorto("Si!!!");
-        __delay_ms(1000);
-        comando(1);
-    } else {
-        contador = 0;
-        meterloEnLaEEPROM(0,1);
-        meterloEnLaEEPROM(1,0);
-        meterloEnLaEEPROM(2,11);
-        meterloEnLaEEPROM(3,11);
-        __delay_ms(1000);
-        comando(1);
+
+    if (RB7 == 0 && RA5 == 0) {
+        mensajeCorto("Ingrese la CLAVE");
+        __delay_ms(500);
+        enviarComando(1);
     }
 
     if (RA0 == 0) {
         if (RB4 == 0) {
-            comando(128 + contador);
+            teclas[contador] = '7';
+            pintarAsteriscos(contador);
             contador++;
-            dato('7');
+            __delay_ms(1000);
         } else if (RB5 == 0) {
-            comando(128 + contador);
+            teclas[contador] = '8';
+            pintarAsteriscos(contador);
             contador++;
-            dato('8');
+            __delay_ms(1000);
         } else if (RB6 == 0) {
-            comando(128 + contador);
+            teclas[contador] = '9';
+            pintarAsteriscos(contador);
             contador++;
-            dato('9');
-        } else if (RB7 == 0) {
-            comando(128 + contador);
-            contador++;
-            dato('/');
+            __delay_ms(1000);
         }
     } else if (RA1 == 0) {
         if (RB4 == 0) {
-            comando(128 + contador);
+            teclas[contador] = '4';
+            pintarAsteriscos(contador);
             contador++;
-            dato('4');
+            __delay_ms(1000);
         } else if (RB5 == 0) {
-            comando(128 + contador);
+            teclas[contador] = '5';
+            pintarAsteriscos(contador);
             contador++;
-            dato('5');
+            __delay_ms(1000);
         } else if (RB6 == 0) {
-            comando(128 + contador);
+            teclas[contador] = '6';
+            pintarAsteriscos(contador);
             contador++;
-            dato('6');
-        } else if (RB7 == 0) {
-            comando(128 + contador);
-            contador++;
-            dato('*');
+            __delay_ms(1000);
         }
     } else if (RA2 == 0) {
         if (RB4 == 0) {
-            comando(128 + contador);
+            teclas[contador] = '1';
+            pintarAsteriscos(contador);
             contador++;
-            dato('1');
+            __delay_ms(1000);
         } else if (RB5 == 0) {
-            comando(128 + contador);
+            teclas[contador] = '2';
+            pintarAsteriscos(contador);
             contador++;
-            dato('2');
+            __delay_ms(1000);
         } else if (RB6 == 0) {
-            comando(128 + contador);
+            teclas[contador] = '3';
+            pintarAsteriscos(contador);
             contador++;
-            dato('3');
-        } else if (RB7 == 0) {
-            comando(128 + contador);
-            contador++;
-            dato('-');
+            __delay_ms(1000);
         }
     } else if (RA3 == 0) {
         if (RB4 == 0) {
-            comando(128 + contador);
-            contador++;
-            dato('S');
+            mensajeCorto("Digite la nueva");
+            banderaNueva = 1;
+            __delay_ms(3000);
+            enviarComando(1);
         } else if (RB5 == 0) {
-            comando(128 + contador);
+            teclas[contador] = '7';
+            pintarAsteriscos(contador);
             contador++;
-            dato('0');
+            __delay_ms(1000);
         } else if (RB6 == 0) {
-            comando(128 + contador);
-            contador++;
-            dato('=');
-        } else if (RB7 == 0) {
-            comando(128 + contador);
-            contador++;
-            dato('+');
+            if (banderaNueva == 1) {
+                if (contador == 4) {
+                    mensajeCorto("Cambiando CLAVE");
+                    EEPROM_Write(0, teclas[0]);
+                    EEPROM_Write(1, teclas[1]);
+                    EEPROM_Write(2, teclas[2]);
+                    EEPROM_Write(3, teclas[3]);
+                    __delay_ms(5000);
+                    enviarComando(1);
+                } else {
+                    mensajeCorto("Longitud ERRONEA");
+                    __delay_ms(3000);
+                    enviarComando(1);
+                }
+
+                banderaNueva = 0;
+            } else {
+                // Compruebo si la clave ingresada corresponde
+                int i = 0;
+                int bandera = 1;
+                while (bandera && i < 4) {
+                    bandera = (teclas[i] == EEPROM_Read(i));
+                    i++;
+                }
+
+                // Indico si la clave ingresada fue la correcta
+                if (bandera == 1 && i == 4) {
+                    mensajeCorto("Clave CORRECTA");
+                    __delay_ms(5000);
+                    enviarComando(1);
+                } else {
+                    contadorErrores++;
+                    mensajeCorto("Clave ERRONEA");
+                    __delay_ms(5000);
+                    enviarComando(1);
+                }
+            }
+
+            teclas[0] = ' ';
+            teclas[1] = ' ';
+            teclas[2] = ' ';
+            teclas[3] = ' ';
+
+            contador = 0;
         }
     }
 
-    GIE = 1;
+    GIE = 1; // Habilitador Global
 }
 
+/**
+ * Función principal del PIC.
+ */
 void main() {
-    nRBPU = 0; // Habilito las resistencias de Pull UP
-
-    TRISA = 0; // Declaro los pines del puerto A como Salida
-    TRISB = 0b11110011; // Declaro los pines de B como Salida y Entrada 
-    TRISC = 0; // Declaro los pines del puerto B como Salida
-
     ADCON1 = 7; // Declaro el puerto A como Digital
 
-    comando(12); // 1100, Display ON - Cursor OFF - Blink OFF
+    TRISA = 0; // Declaro los pines del puerto A como Salida
+    TRISB = 241; // 11110001, Declaro los pines de B como Salida y Entrada 
+    TRISC = 0; // Declaro los pines del puerto C como Salida
 
-    GIE = 1;
-    RBIF = 0;
-    INTF = 0;
-    INTE = 1;
-    RBIE = 1;
+    enviarComando(12); // 1100, Display ON - Cursor OFF - Blink OFF
+
+    nRBPU = 0; // Habilito las resistencias de Pull UP
+
+    GIE = 1; // Habilitador Global
+    INTF = 0; // Bandera de RB0
+    RBIF = 0; // Bandera del Puerto B
+    INTE = 1; // Habilitador de RB0
+    RBIE = 1; // Habilitador del Puerto B
+
+    SYNC = 0; // Sincronia-1, Asincronia-0
+    BRGH = 1; // Velocidad de Transmision: 0-Alta, 1-Baja
+    SPBRG = 25; // Bits de configuración del puerto Serial
+
+    if (EEPROM_Read(0) == 255 || EEPROM_Read(3) == 255) {
+        mensajeCorto("EEPROM vacia");
+        __delay_ms(3000);
+        enviarComando(1);
+
+        mensajeCorto("Escribiendo...");
+        EEPROM_Write(0, '1');
+        EEPROM_Write(1, '2');
+        EEPROM_Write(2, '3');
+        EEPROM_Write(3, '4');
+        __delay_ms(2000);
+        enviarComando(1);
+    } else {
+        mensajeCorto("EEPROM con clave");
+        __delay_ms(3000);
+        enviarComando(1);
+
+        mensajeCorto("Leyendo...");
+        __delay_ms(2000);
+        enviarComando(1);
+    }
 
     while (1) {
-        PORTA = 14;
+        PORTA = 14; // 001110
         __delay_ms(100);
-        PORTA = 13;
+        PORTA = 45; // 101101
         __delay_ms(100);
-        PORTA = 11;
+        PORTA = 11; // 001011
         __delay_ms(100);
-        PORTA = 7;
+        PORTA = 39; // 100111
         __delay_ms(100);
+
+        while (contadorErrores >= 3) {
+            mensajeCorto("SecurityBox Lock");
+            SPEN = 1; // Habilitador del puerto serial
+            transmitirMensaje(" SecurityBox Lock ");
+            SPEN = 0; // Habilitador del puerto serial
+        }
     };
 }
